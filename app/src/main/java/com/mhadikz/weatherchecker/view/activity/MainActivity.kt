77c54fb5052e92998f36ccc20b8d8fa12ac66d4d
mhadikz <mhkaramzadeh7@gmail.com>
2021-02-12 23:13:20 +0330

    Update main activity for fetching data from the repository
    Also, add the bottom pager

--- app/src/main/java/com/mhadikz/weatherchecker/view/activity/MainActivity.kt
+++ app/src/main/java/com/mhadikz/weatherchecker/view/activity/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.mhadikz.weatherchecker.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.mhadikz.weatherchecker.R
-import com.mhadikz.weatherchecker.adapter.DateWeatherRcAdapter
 import com.mhadikz.weatherchecker.adapter.ForecastDaysAdapter
-import com.mhadikz.weatherchecker.adapter.GenericAdapter
 import com.mhadikz.weatherchecker.databinding.ActivityMainBinding
-import com.mhadikz.weatherchecker.databinding.TodayFragmentBinding
-import com.mhadikz.weatherchecker.model.DateWeather
+import com.mhadikz.weatherchecker.model.ForecastWeatherData
+import com.mhadikz.weatherchecker.util.DateUtil
+import com.mhadikz.weatherchecker.util.Status
+import com.mhadikz.weatherchecker.util.TemperatureUtil
+import com.mhadikz.weatherchecker.view.base.BaseActivity
+import com.mhadikz.weatherchecker.view.viewModel.ViewModelFactory
 import com.mhadikz.weatherchecker.view.fragment.SevenDaysFragment
 import com.mhadikz.weatherchecker.view.fragment.TodayFragment
 import com.mhadikz.weatherchecker.view.fragment.TomorrowFragment
+import com.mhadikz.weatherchecker.view.viewModel.MainViewModel
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
@@ -33,17 +30,44 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override fun setViewModelClass(): Class<MainViewModel> = MainViewModel::class.java
 
     override fun initObservers() {
-        // TODO: Implement the initObservers
+        viewModel.getForecastWeather("31.3203", "48.6693").observe(this, {
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.START -> {
+                    }
+                    Status.SUCCESS -> {
+                        resource.data?.let { data ->
+                            setValues(data)
+                            binding?.loading?.visibility = View.GONE
+                            binding?.mainLayout?.visibility = View.VISIBLE
+                        }
+
+                    }
+                    Status.ERROR -> {
+                        binding?.loading?.visibility = View.GONE
+                        binding?.error?.visibility = View.VISIBLE
+                    }
+                    Status.LOADING -> {
+                    }
+                    Status.EMPTY -> {
+                        binding?.loading?.visibility = View.GONE
+                        binding?.error?.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
     }
 
-    override fun setViewModelFactory(): Nothing? = null
 
+    override fun setViewModelFactory(): ViewModelProvider.Factory {
+        return ViewModelFactory()
+    }
 
     override fun onBindComponentsValues() {
-        // TODO: Implement the onBindComponentsValues
+
     }
 
-    private fun initPageAdapter(){
+    private fun initPageAdapter() {
         val adapter = ForecastDaysAdapter(supportFragmentManager)
         adapter.addFragment(TodayFragment(), getString(R.string.today))
         adapter.addFragment(TomorrowFragment(), getString(R.string.tomorrow))
@@ -51,4 +75,13 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
         binding?.viewPager?.adapter = adapter
         binding?.tabLayout?.setupWithViewPager(binding?.viewPager)
     }
+
+    private fun setValues(forecastWeatherData: ForecastWeatherData) {
+        binding?.forecastWeatherData = forecastWeatherData
+        binding?.txtCityName?.text = forecastWeatherData.timezone
+        binding?.txtTemperatureNumber?.text =
+            TemperatureUtil.convertKelvinToCelsius(forecastWeatherData.current.temp).toString()
+        binding?.txtDate?.text = DateUtil.getDateString(forecastWeatherData.current.dt)
+    }
+
 }
