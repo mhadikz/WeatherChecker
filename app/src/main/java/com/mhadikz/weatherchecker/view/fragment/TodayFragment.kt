4350e20069c8371265ea1e7a7c4233edc961699e
mhadikz <mhkaramzadeh7@gmail.com>
2021-02-12 23:16:22 +0330

    Update base abstract classes and fragments

--- app/src/main/java/com/mhadikz/weatherchecker/view/fragment/TodayFragment.kt
+++ app/src/main/java/com/mhadikz/weatherchecker/view/fragment/TodayFragment.kt
@@ -1,13 +1,17 @@
 package com.mhadikz.weatherchecker.view.fragment
 
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.mhadikz.weatherchecker.R
 import com.mhadikz.weatherchecker.databinding.TodayFragmentBinding
 import com.mhadikz.weatherchecker.view.base.BaseFragment
+import com.mhadikz.weatherchecker.view.viewModel.MainViewModel
 import com.mhadikz.weatherchecker.view.viewModel.TodayViewModel
+import com.mhadikz.weatherchecker.view.viewModel.ViewModelFactory
 
 class TodayFragment : BaseFragment<TodayViewModel, TodayFragmentBinding>() {
 
-    override fun setViewModelClass() = TodayViewModel::class.java
+    override fun setViewModelClass(): Class<TodayViewModel> = TodayViewModel::class.java
 
 
     override fun layoutId(): Int {
@@ -15,18 +19,17 @@ class TodayFragment : BaseFragment<TodayViewModel, TodayFragmentBinding>() {
     }
 
     override fun initComponents() {
-        // TODO: Implement the initComponents
     }
 
     override fun initObservers() {
-        // TODO: Implement the initObservers
     }
 
     override fun onBindComponentsValues() {
         // TODO: Implement the onBindComponentsValues
     }
 
-    override fun setViewModelFactory(): Nothing? = null
-
+    override fun setViewModelFactory(): ViewModelProvider.Factory {
+        return ViewModelFactory()
+    }
 
 }
--- app/src/main/java/com/mhadikz/weatherchecker/view/fragment/TomorrowFragment.kt
+++ app/src/main/java/com/mhadikz/weatherchecker/view/fragment/TomorrowFragment.kt
@@ -1,13 +1,15 @@
 package com.mhadikz.weatherchecker.view.fragment
 
+import androidx.lifecycle.ViewModelProvider
 import com.mhadikz.weatherchecker.R
 import com.mhadikz.weatherchecker.databinding.TomorrowFragmentBinding
 import com.mhadikz.weatherchecker.view.base.BaseFragment
 import com.mhadikz.weatherchecker.view.viewModel.TomorrowViewModel
+import com.mhadikz.weatherchecker.view.viewModel.ViewModelFactory
 
 class TomorrowFragment : BaseFragment<TomorrowViewModel, TomorrowFragmentBinding>() {
 
-    override fun setViewModelClass() = TomorrowViewModel::class.java
+    override fun setViewModelClass(): Class<TomorrowViewModel> = TomorrowViewModel::class.java
 
     override fun layoutId(): Int {
         return R.layout.tomorrow_fragment
@@ -21,8 +23,9 @@ class TomorrowFragment : BaseFragment<TomorrowViewModel, TomorrowFragmentBinding
         // TODO: Implement the initObservers
     }
 
-    override fun setViewModelFactory(): Nothing? = null
-
+    override fun setViewModelFactory(): ViewModelProvider.Factory {
+        return ViewModelFactory()
+    }
     override fun onBindComponentsValues() {
         // TODO: Implement the onBindComponentsValues
     }
